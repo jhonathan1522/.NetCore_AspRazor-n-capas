1d30fd033f53f3086cc6248b957f63456bad1114
Jhonathan1522 <jhonathan.rodriguez1522@gmail.com>
2021-10-11 18:11:54 -0500

    Se agrega clase de consulta en webApi

---
+++ Aplicacion/Empleados/Consulta.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using System.Threading;
+using Persistencia;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aplicacion.Empleados
+{
+    public class Consulta
+    {
+        public class ListaEmpleados : IRequest<List<Empleado>> {}
+
+        public class Manejador : IRequestHandler<ListaEmpleados, List<Empleado>>
+        {
+            private readonly ApplicationContext _context;
+            public Manejador(ApplicationContext context) {
+                _context = context;
+            }
+            public async Task<List<Empleado>> Handle(ListaEmpleados request, CancellationToken cancellationToken)
+            {
+                var cursos = await _context.Empleados.ToListAsync();
+                return cursos;
+            }
+        }
+
+    }
+}
