e3e2c1fa540927b97f2ba1d2502620a7adaa3a88
Jhonathan1522 <jhonathan.rodriguez1522@gmail.com>
2021-10-12 15:26:45 -0500

    Crear nuevo empleado en el mediador

---
+++ Aplicacion/Empleados/Nuevo.cs
@@ -0,0 +1,59 @@
+﻿using Dominio;
+using MediatR;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Empleados
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest {
+
+            public string Nombre { get; set; }
+
+            public int Edad { get; set; }
+
+            public float Altura { get; set; }
+        };
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+
+            private readonly ApplicationContext _context;
+            public Manejador(ApplicationContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var empleado = new Empleado
+                {
+                    Nombre = request.Nombre,
+                    Edad = request.Edad,
+                    Altura = request.Altura
+                };
+
+                _context.Empleados.Add(empleado);
+
+                // Si devuelve 0 no realizo la transacción 
+                var valor = await _context.SaveChangesAsync();
+
+                if (valor > 0) {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo insertar el empleado");
+
+            }
+        }
+
+
+
+
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -50,7 +50,7 @@ namespace WebAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
